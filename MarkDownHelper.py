e5d70c4ccac21e1b7a25bd0843ec99eec1fb7955
allen <437806668@qq.com>
2016-06-24 11:57:51 +0800

    fix bug

--- MarkDownHelper.py
+++ MarkDownHelper.py
@@ -6,52 +6,62 @@ import qiniu.config
 from qiniu import BucketManager
 import sys
 import os
+import msvcrt
+import datetime
+import subprocess
 
-'''
-allen's script for uploading img to qiniu and get md_url
-'''
+
+
+# you will get md_url in this file
+result_file = "ss.txt"  
+
+if os.path.exists(result_file):
+    os.remove(result_file)
+os.chdir(sys.path[0])
 
 
 # you need get yours msg here
 access_key = "XXXX"
-secret_key = "XXXX"
-bucket_name = 'XXXX'
-bucket_url = 'XXXX'
+secret_key =  "XXXX"
+bucket_name =  "XXXX"
+bucket_url =  "XXXX"
 md_url_result = "md_url.txt"
 
-# upload your img to qiniu
+img_suffix = ["jpg", "jpeg", "png", "bmp", "gif"]
+
 def upload_img(bucket_name,file_name,file_path):
     # generate token
     token = q.upload_token(bucket_name, file_name, 3600)
     info = put_file(token, file_name, file_path)
-    # delete local img file and if you do not want to delete local img,you just need to delete "os.remove(file_path)"
+    # delete local imgFile
     os.remove(file_path)
     return
 
-# get md_url 
 def get_img_url(bucket_url,file_name):
 
     img_url = 'http://%s/%s' % (bucket_url,file_name)
     # generate md_url
     md_url = "![%s](%s)\n" % (file_name, img_url)
+
     return md_url
 
-# save to txt
+
 def save_to_txt(bucket_url,file_name):
-    # get url
+
     url_before_save = get_img_url(bucket_url,file_name)
     # save to clipBoard
     addToClipBoard(url_before_save)
-    
     # save md_url to txt
     with open(md_url_result, "a") as f:
         f.write(url_before_save)
+
     return
-   
+
 # save to clipboard
 def addToClipBoard(text):
 	command = 'echo ' + text.strip() + '| clip'
 	os.system(command)
+
 
 
 if __name__ == '__main__':
@@ -60,9 +70,11 @@ if __name__ == '__main__':
     bucket = BucketManager(q)
 
     imgs = sys.argv[1:]
-    # drop more than one img to .py file and
-    # it will auto run and generate txt file
+
     for img in imgs:
+    
         up_filename = os.path.split(img)[1]
+
         upload_img(bucket_name,up_filename,img)
+
         save_to_txt(bucket_url,up_filename)
