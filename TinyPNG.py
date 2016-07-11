c9a85403b1264f86cd6d85cf58ed9363fa773fd9
allen <437806668@qq.com>
2016-07-12 07:52:41 +0800

    利用 TinyPNG 压缩并且指定图片尺寸得脚本

---
+++ TinyPNG.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+
+import tinify,os
+import sys
+import os
+
+
+'''
+参数配置很简单:https://tinypng.com/developers/reference/python
+1:注册获取 key 填入 xxxx 中
+2:pip install --upgrade tinify
+'''
+
+
+
+# you need get yours msg here
+tinify.key = "xxxx"
+
+
+if __name__ == '__main__':
+
+    imgs = sys.argv[1:]
+
+    for img in imgs:
+
+        source = tinify.from_file(img)
+        
+	resized = source.resize(
+ 	   method="fit",
+    	   width=200,
+           height=300
+	)
+
+        os.remove(img)
+	resized.to_file(img)
